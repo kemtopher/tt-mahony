335e87a8f70b5e12f94e150cb321b4c530cac534
Chris Kemeza <ckemeza1@gmail.com>
2025-05-02 13:54:08 -0400

    initial site setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,85 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
 module.exports = {
   siteMetadata: {
     title: `TT Mahony's Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.ttmahony.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-netlify",
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: "",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: false,
+    //     // Setting this parameter is optional
+    //     // anonymize: true,
+    //     // Setting this parameter is also optional
+    //     // respectDNT: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 0,
+    //     // Enables Google Optimize using your container Id
+    //     // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+    //     // Enables Google Optimize Experiment ID
+    //     // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+    //     // Set Variation ID. 0 for original 1,2,3....
+    //     // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+    //     // Defers execution of google analytics script after page load
+    //     defer: false,
+    //     // Any additional optional fields
+    //     sampleRate: 5,
+    //     siteSpeedSampleRate: 10,
+    //     cookieDomain: "ttmahony.com",
+    //     // defaults to false
+    //     enableWebVitalsTracking: true,
+    //   },
+    // },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN
+        // routes,
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        mode: "render-blocking",
+        enableListener: true,
+        preconnect: [
+          // `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Oswald`,
+            file: `https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;600&display=swap`,
+          },
+          {
+            name: `DM Serif Display`,
+            file: `https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap`,
+          },
+          {
+            name: `Merriweather`,
+            file: `https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;1,300;1,400&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
